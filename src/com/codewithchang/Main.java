b753b7ba31bc7888339df9206044c1db92c1d615
Chang <cnxiong03@gmail.com>
2021-01-04 00:03:51 -0500

    added comments

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // a mile is equal to 1.609344 kilometers
-        double kilometers = (100 * 1.609344);
-        int highScore = 50;
+        double kilometers = (100 * 1.609344); // an expression is everything from kilometers excluding the semi-colon
+        int highScore = 50; // highScore = 50 is the expression
 
-        if (highScore == 50) {
+        if (highScore == 50) { // everything inside of the parentheses is an expression
             System.out.println("This is an expression");
         }
     }
